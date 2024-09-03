185b59c6bd58b0d052a3a68b5e1195685b4371c5
Victor Ndou <victor.ndou@redacademy.co.za>
2024-09-03 15:02:24 +0200

    Worked on Views, Models and Controllers

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Victor",
+                DateOfBirth = new DateTime(1999,02,10)
+            };
+            return View(data);
+        }
+    }
+}
